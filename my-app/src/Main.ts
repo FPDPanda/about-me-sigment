272734d0e943e24041a247f5af74fd5be59ba074
Jean Jacques <35910266+FPDPanda@users.noreply.github.com>
2025-09-15 23:01:38 -0300

    TODO: Create body

--- my-app/src/Main.ts
+++ my-app/src/Main.ts
@@ -2,13 +2,12 @@ import { MyApp, mount } from "sigment";
 import Header from "./components/header/Header";
 import Footer from "./components/Footer";
 import "./assets/css/index.css";
-import Body from "./components/Body";
 
 MyApp.cleanHtml(true); //in development use false in production use true
 MyApp.setMaxCacheSize(50); // cache 50 components
 
 async function Main() {
-  const app = fragment(Header(), await Body(), Footer());
+  const app = fragment(Header() /*, await Body()*/, Footer());
 
   mount("root", app);
 }

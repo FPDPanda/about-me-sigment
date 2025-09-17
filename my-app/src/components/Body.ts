704c31ed7d510266cdc2c922a4ef37e19f5b2744
Jean Jacques <35910266+FPDPanda@users.noreply.github.com>
2025-09-17 20:45:47 -0300

    Creating ArticlesService

--- my-app/src/components/Body.ts
+++ my-app/src/components/Body.ts
@@ -1,6 +1,12 @@
+import Articles from "./articles/Articles";
 import Home from "./home/Home";
 
 function Body() {
+  const path = window.location.pathname;
+
+  if (path.split("/").pop() == "articles") {
+    return Articles();
+  }
   return Home();
 }
 
---
+++ my-app/src/entities/articles.ts
@@ -0,0 +1,8 @@
+import Article from "./article";
+
+interface Articles {
+  title: string;
+  articles: Article[];
+}
+
+export default Articles;
